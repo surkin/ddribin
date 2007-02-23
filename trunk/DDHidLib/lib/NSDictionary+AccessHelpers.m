1b35a3bf93c8931c0b7dc789168092cc0673b3e7
ddribin <ddribin@4d274d4c-122a-0410-acac-7763e38d2750>
2007-02-23 05:50:01 +0000

    Prefix NSDictionary category selectors with 'ddhid_'

--- trunk/DDHidLib/lib/NSDictionary+AccessHelpers.m
+++ trunk/DDHidLib/lib/NSDictionary+AccessHelpers.m
@@ -11,38 +11,38 @@
 
 @implementation NSDictionary (AccessHelpers)
 
-- (unsigned) unsignedForKey: (NSString *) key;
+- (unsigned) ddhid_unsignedForKey: (NSString *) key;
 {
     NSNumber * number = [self objectForKey: key];
     return [number unsignedIntValue];
 }
 
-- (id) objectForString: (const char *) key;
+- (id) ddhid_objectForString: (const char *) key;
 {
     NSString * objcKey = [NSString stringWithCString: key];
     return [self objectForKey: objcKey];
 }
 
-- (NSString *) stringForString: (const char *) key;
+- (NSString *) ddhid_stringForString: (const char *) key;
 {
-    return [self objectForString: key];
+    return [self ddhid_objectForString: key];
 }
 
-- (long) longForString: (const char *) key;
+- (long) ddhid_longForString: (const char *) key;
 {
-    NSNumber * number =  [self objectForString: key];
+    NSNumber * number =  [self ddhid_objectForString: key];
     return [number longValue];
 }
 
-- (unsigned int) unsignedIntForString: (const char *) key;
+- (unsigned int) ddhid_unsignedIntForString: (const char *) key;
 {
-    NSNumber * number =  [self objectForString: key];
+    NSNumber * number =  [self ddhid_objectForString: key];
     return [number unsignedIntValue];
 }
 
-- (BOOL) boolForString: (const char *) key;
+- (BOOL) ddhid_boolForString: (const char *) key;
 {
-    NSNumber * number =  [self objectForString: key];
+    NSNumber * number =  [self ddhid_objectForString: key];
     return [number boolValue];
 }
 
@@ -50,7 +50,7 @@
 
 @implementation NSMutableDictionary (AccessHelpers)
 
-- (void) setObject: (id) object forString: (const char *) key;
+- (void) ddhid_setObject: (id) object forString: (const char *) key;
 {
     NSString * objcKey = [NSString stringWithCString: key];
     [self setObject: object forKey: objcKey];
