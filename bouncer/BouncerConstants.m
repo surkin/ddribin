cc2b8b78942726d81878d5ab2510ebda6e9c3afd
ddribin <ddribin@4d274d4c-122a-0410-acac-7763e38d2750>
2007-08-10 02:35:10 +0000

    Add BSD notification to automatically start playback

---
+++ bouncer/BouncerConstants.m
@@ -0,0 +1,15 @@
+//
+//  BouncerConstants.m
+//  TheBouncer
+//
+//  Created by Dave Dribin on 8/9/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "BouncerConstants.h"
+
+NSString * BouncerDisableKey = @"org.dribin.dave.TheBouncer.disabled";
+NSString * BouncerConnectionFormat = @"DDBouncerVictimDO %@";
+NSString * BouncerDOAvailableNotification = @"DDBouncerDOAvailable";
+NSString * BouncerDOGoneNotification = @"DDBouncerDOGone";
+const char * BouncerRemoteStartNotification = "org.dribin.dave.TheBouncer.DDRemoteStart";
