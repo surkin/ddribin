1af3fb8e7e9eba752a9318e2a6fcbad57ec14820
ddribin <ddribin@4d274d4c-122a-0410-acac-7763e38d2750>
2008-05-04 16:13:28 +0000

    Remove ddcli.  It's now hosted in my own Mercurial repo

--- trunk/ddcli/doc/runWithArguments.m
+++
@@ -1,7 +0,0 @@
-- (int) application: (DDCliApplication *) app
-   runWithArguments: (NSArray *) arguments;
-{
-    ddprintf(@"Output: %@, help: %d\n", _output, _help);
-    ddprintf(@"Arguments: %@\n", arguments);
-    return 0;
-}
